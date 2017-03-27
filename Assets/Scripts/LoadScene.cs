2a3defd6c73327339f78947880bd570d077e76c4
BlackHydra2112 <krumnowm@gmatc.matc.edu>
2017-03-27 16:05:04 -0500

    Build Ready
    Most Updated Commit 3/27/2017

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+    public bool isStart;
+    public bool isQuit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+        {
+            Application.LoadLevel(1);
+        }
+        if (isQuit)
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(1);
+    }
+}
