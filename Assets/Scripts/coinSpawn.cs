74c3593498aa1ad1492acb106841bcec58db2b1f
BlackHydra2112 <krumnowm@gmatc.matc.edu>
2017-05-10 18:12:05 -0500

    merge hope

---
+++ Assets/Scripts/coinSpawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinSpawn : MonoBehaviour {
+    public GameObject[] coins;
+	// Use this for initialization
+	void Start () {
+        int rand = Random.Range(0, coins.Length);
+        Instantiate(coins[rand], transform.position, transform.rotation);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
